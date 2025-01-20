d69de0438c384ec1492f454c18c65ac321574016
Rafael Lodi <rafaelcardeallodi@hotmail.com>
2025-01-20 17:11:20 -0300

    Cancellation and retry

--- src/components/upload-widget-upload-item.tsx
+++ src/components/upload-widget-upload-item.tsx
@@ -2,17 +2,21 @@ import * as Progress from '@radix-ui/react-progress'
 import { Download, ImageUp, Link2, RefreshCcw, X } from 'lucide-react'
 import { motion } from 'motion/react'
 
-import type { Upload } from '../store/uploads'
+import { type Upload, useUploads } from '../store/uploads'
 import { formatBytes } from '../utils/format-bytes'
 import { Button } from './ui/button'
 
 interface UploadWidgetUploadItemProps {
+  uploadId: string
   upload: Upload
 }
 
 export function UploadWidgetUploadItem({
+  uploadId,
   upload,
 }: UploadWidgetUploadItemProps) {
+  const cancelUpload = useUploads(store => store.cancelUpload)
+
   return (
     <motion.div
       className="relative flex flex-col gap-3 overflow-hidden rounded-lg bg-white/2 p-3 shadow-shape-content"
@@ -34,36 +38,53 @@ export function UploadWidgetUploadItem({
             <span className="ml-1 text-green-400">-94%</span>
           </span>
           <div className="size-1 rounded-full bg-zinc-700" />
-          <span>45%</span>
+          {upload.status === 'progress' && <span>45%</span>}
+          {upload.status === 'success' && <span>100%</span>}
+          {upload.status === 'error' && (
+            <span className="text-red-400">Error</span>
+          )}
+          {upload.status === 'canceled' && (
+            <span className="text-yellow-400">Canceled</span>
+          )}
         </span>
       </div>
 
-      <Progress.Root className="h-1 overflow-hidden rounded-full bg-zinc-800">
+      <Progress.Root
+        data-status={upload.status}
+        className="group h-1 overflow-hidden rounded-full bg-zinc-800"
+      >
         <Progress.Indicator
-          className="h-1 bg-indigo-500"
+          className="h-1 bg-indigo-500 group-data-[status='canceled']:bg-yellow-400 group-data-[status='error']:bg-red-400 group-data-[status='success']:bg-green-400"
           style={{
-            width: '43%',
+            width: upload.status === 'progress' ? '45%' : '100%',
           }}
         />
       </Progress.Root>
 
       <div className="absolute top-2.5 right-2.5 flex items-center gap-1">
-        <Button size="icon-sm">
+        <Button disabled={upload.status !== 'success'} size="icon-sm">
           <Download className="size-4" strokeWidth={1.5} />
           <span className="sr-only">Download compress image</span>
         </Button>
 
-        <Button size="icon-sm">
+        <Button disabled={upload.status !== 'success'} size="icon-sm">
           <Link2 className="size-4" strokeWidth={1.5} />
           <span className="sr-only">Copy remote URL</span>
         </Button>
 
-        <Button size="icon-sm">
+        <Button
+          disabled={!['canceled', 'error'].includes(upload.status)}
+          size="icon-sm"
+        >
           <RefreshCcw className="size-4" strokeWidth={1.5} />
           <span className="sr-only">Retry upload</span>
         </Button>
 
-        <Button size="icon-sm">
+        <Button
+          disabled={upload.status !== 'progress'}
+          size="icon-sm"
+          onClick={() => cancelUpload(uploadId)}
+        >
           <X className="size-4" strokeWidth={1.5} />
           <span className="sr-only">Cancel upload</span>
         </Button>
--- src/components/upload-widget-upload-list.tsx
+++ src/components/upload-widget-upload-list.tsx
@@ -17,7 +17,13 @@ export function UploadWidgetUploadList() {
       ) : (
         <div className="flex flex-col gap-2">
           {Array.from(uploads.entries()).map(([uploadId, upload]) => {
-            return <UploadWidgetUploadItem key={uploadId} upload={upload} />
+            return (
+              <UploadWidgetUploadItem
+                key={uploadId}
+                uploadId={uploadId}
+                upload={upload}
+              />
+            )
           })}
         </div>
       )}
