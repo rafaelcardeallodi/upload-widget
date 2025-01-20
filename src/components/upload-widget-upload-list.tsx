9b4fedf9d811cebe924f61a0fe5e35262f786e77
Rafael Lodi <rafaelcardeallodi@hotmail.com>
2025-01-20 09:38:50 -0300

    Create UploadItem component

--- src/components/upload-widget-upload-list.tsx
+++ src/components/upload-widget-upload-list.tsx
@@ -1,3 +1,16 @@
+import { UploadWidgetUploadItem } from './upload-widget-upload-item'
+
 export function UploadWidgetUploadList() {
-  return <div className="h-48 px-3"></div>
+  return (
+    <div className="flex flex-col gap-3 px-3">
+      <span className="font-medium text-xs">
+        Uploaded files <span className="text-zinc-400">(3)</span>
+      </span>
+
+      <div className="flex flex-col gap-2">
+        <UploadWidgetUploadItem />
+        <UploadWidgetUploadItem />
+      </div>
+    </div>
+  )
 }
